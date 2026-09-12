d2f81c2155c68456f444949ba9e68a0f97c4c5da
leagomosupi <whoistarboy@icloud.com>
2026-09-13 00:15:15 +0200

    Add files via upload

---
+++ my-portfolio/script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+    document.querySelector("#projects").scrollIntoView({
+        behavior: "smooth"
+    });
+});
